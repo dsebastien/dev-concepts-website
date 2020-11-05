b0ecb57770ab6ac5ef283716028e6f5067db2196
Sebastien Dubois <seb@dsebastien.net>
2020-11-05 04:15:15 +0100

    Made the title align correctly with the tagline

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,11 +4,11 @@ import { graphql, useStaticQuery } from "gatsby";
 import { Code } from "../code";
 
 const StyledHeader = styled.div.attrs({
-  className: "w-full mx-auto mt-0 py-6 flex flex-col md:flex-row md:justify-between items-center",
+  className: "w-full mx-auto text-left sm:text-center mt-0 py-6 flex flex-col md:flex-row md:justify-between items-center",
 })``;
 
 const Tagline = styled.h2.attrs({
-  className: "w-full text-left sm:text-center block md:hidden",
+  className: "w-full mx-auto text-left sm:text-center block md:hidden",
 })``;
 
 const command = "sudo make me a Software Craftsman";
@@ -37,7 +37,7 @@ const Header: React.FC = () => {
   return (
     <StyledHeader>
       {/*<Link to="/about">About</Link>*/}
-      <h1>
+      <h1 className="w-full">
         <a href={data.site.siteMetadata.salesPageUrl} title="Pre-order the book now!" aria-label="Pre-order the book now!">
           {data.site.siteMetadata.title}
         </a>
